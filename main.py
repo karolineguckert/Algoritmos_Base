d29e4008504bdf608d9c19291fef99f5c9c82672
KarolineGuckert <karolinesouzaguckert@gmail.com>
2023-03-13 20:24:49 -0300

    transforming in methods

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import cv2 as cv2
 from skimage import io
 
-path_img_01 = 'images/images.jpeg'
-image01 = io.imread(path_img_01)
-image02 = io.imread(path_img_01)
-image02 = cv2.cvtColor(image02, cv2.COLOR_BGR2RGB)
+def convertImageToGray(image):
+    print("a")
 
+def main():
+    path_img_01 = 'images/images.jpeg'
+    image01 = io.imread(path_img_01)
+    image02 = io.imread(path_img_01)
+    image02 = cv2.cvtColor(image02, cv2.COLOR_BGR2RGB)
 
-image02[0] = (0, 0, 0)
-cv2.imshow("imagem 02", image02)
-cv2.imshow("imagem 01", image01)
-cv2.waitKey(0)
+    image02[0] = (0, 0, 0)
+    convertImageToGray(image02)
+    cv2.imshow("imagem 02", image02)
+    cv2.imshow("imagem 01", image01)
+    cv2.waitKey(0)
+
+main()
