d7292718aa6523de8fe004cb772892ce677122b4
Matheus S. Heck <matheus@qualitys.com.br>
2023-03-13 21:57:45 -0300

    Creating new method to convert image to grayscale weighted

--- main.py
+++ main.py
@@ -22,8 +22,22 @@ def convert2Gray(image):
 
             (r, g, b) = image[i, j]
 
+            img[i, j] = (r*float(0.299) + g*float(0.587) + b*float(0.114))
+    cv.imshow("grayscale 1", img)
+
+
+def convert2GrayWeighted(image):
+    img = cv.cvtColor(image, cv.COLOR_BGR2RGB)
+
+    for i in range(image.shape[0]):
+        for j in range(image.shape[1]):
+
+            (r, g, b) = image[i, j]
+
             img[i, j] = (r*float(0.299) + g*float(0.587) + b*float(0.114))/3
-    cv.imshow("nova imagem", img)
+    cv.imshow("grasycale weighted", img)
+
+
 
 def main():
     path_img_01 = 'images/images.jpeg'
@@ -32,6 +46,7 @@ def main():
     image02 = cv.cvtColor(image02, cv.COLOR_BGR2RGB)
 
     convert2Gray(image02)
+    convert2GrayPonderada(image02)
     #cv.imshow("imagem 02", image02)
     cv.imshow("imagem 01", image01)
     cv.waitKey(0)
