d9253224ed046072fa94c63345a809180a703adf
KarolineGuckert <karolinesouzaguckert@gmail.com>
2023-03-13 22:07:58 -0300

    adjusting methods

--- main.py
+++ main.py
@@ -1,42 +1,29 @@
 import cv2 as cv
 from skimage import io
 
-def convertImageToGray(image01):
-    print("a")
 
-    image03 = cv.cvtColor(image01, cv.COLOR_BGR2RGB)
+def convertToGray(image):
+    auxImage = cv.cvtColor(image, cv.COLOR_BGR2RGB)
+    height = image.shape[0]
+    width = image.shape[1]
 
-    for i in range(0, image01.shape[0]):
-        for j in range(0, image01.shape[1]):
-            pixel01 = image01[i, j]
-            pixel02 = 0.299 * pixel01[0] + 0.587 * pixel01[1] + 0.114 * pixel01[2]
-            image03[i,j] = pixel02
-
-    cv.imshow(image03)
-
-def convert2Gray(image):
-    img = cv.cvtColor(image, cv.COLOR_BGR2RGB)
-
-    for i in range(image.shape[0]):
-        for j in range(image.shape[1]):
-
+    for i in range(height):
+        for j in range(width):
             (r, g, b) = image[i, j]
+            auxImage[i, j] = r * float(0.299) + g * float(0.587) + b * float(0.114)
+    cv.imshow("grayscale 1", auxImage)
 
-            img[i, j] = (r*float(0.299) + g*float(0.587) + b*float(0.114))
-    cv.imshow("grayscale 1", img)
 
+def convertToGrayWeighted(image):
+    auxImage = cv.cvtColor(image, cv.COLOR_BGR2RGB)
+    height = image.shape[0]
+    width = image.shape[1]
 
-def convert2GrayWeighted(image):
-    img = cv.cvtColor(image, cv.COLOR_BGR2RGB)
-
-    for i in range(image.shape[0]):
-        for j in range(image.shape[1]):
-
+    for i in range(height):
+        for j in range(width):
             (r, g, b) = image[i, j]
-
-            img[i, j] = (r*float(0.299) + g*float(0.587) + b*float(0.114))/3
-    cv.imshow("grasycale weighted", img)
-
+            auxImage[i, j] = (r * float(0.299) + g * float(0.587) + b * float(0.114)) / 3
+    cv.imshow("grasycale weighted", auxImage)
 
 
 def main():
@@ -45,10 +32,11 @@ def main():
     image02 = io.imread(path_img_01)
     image02 = cv.cvtColor(image02, cv.COLOR_BGR2RGB)
 
-    convert2Gray(image02)
-    convert2GrayPonderada(image02)
-    #cv.imshow("imagem 02", image02)
+    convertToGray(image02)
+    convertToGrayWeighted(image02)
+    # cv.imshow("imagem 02", image02)
     cv.imshow("imagem 01", image01)
     cv.waitKey(0)
+
 
 main()
