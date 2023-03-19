7e13fa0502dc4e48ad2427db514b7b6fe7f61442
Matheus S. Heck <matheus@qualitys.com.br>
2023-03-19 11:04:50 -0300

    Creating comments

--- main.py
+++ main.py
@@ -2,31 +2,42 @@ import cv2 as cv
 from skimage import io
 import numpy as np
 
+#Function to convert a color image to grayscale
+#using arithmetic method
 def convertToGray(image):
-    auxImage = np.zeros((image.shape[0], image.shape[1], 1), np.uint8)
-    height = image.shape[0]
-    width = image.shape[1]
+    auxImage = np.zeros((image.shape[0], image.shape[1], 1), np.uint8) #create a new image with 1 channel
+
+    height = image.shape[0] #takes the number of columns
+    width = image.shape[1]  #takes the number of lines
 
     for i in range(height):
         for j in range(width):
 
+            #sum all the color values and multiply to 0.33 to convert to grayscale
             auxImage[i, j] = sum(image[i, j]) * 0.33
 
     cv.imshow("grayscale - arithmetic", auxImage)
 
-
+#Function to convert a color image to grayscale
+#using weighted method
 def convertToGrayWeighted(image):
-    auxImage = np.zeros((image.shape[0], image.shape[1], 1), np.uint8)
+    auxImage = np.zeros((image.shape[0], image.shape[1], 1), np.uint8) #create a new image with 1 channel
 
-    height = image.shape[0]
-    width = image.shape[1]
+    height = image.shape[0] #takes the number of columns
+    width = image.shape[1]  #takes the number of lines
 
     for i in range(height):
         for j in range(width):
+            #get the color of each color channel in the pixel specified
             (r, g, b) = image[i, j]
+
+            # multiply each color channel and sum all the values to convert to grayscale
             auxImage[i, j] = (r * float(0.299) + g * float(0.587) + b * float(0.114))
+
     cv.imshow("grasycale - weighted", auxImage)
 
+
+def
 
 def main():
     path_img_01 = 'images/images.jpeg'
