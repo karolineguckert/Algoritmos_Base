99f2c89bcacdf29ea7d79ac7d5ce14cefec3f996
GFV <guilhermeferreiravieira@outlook.com>
2023-03-20 21:46:56 -0300

    adicao

--- aritmeticas.py
+++ aritmeticas.py
@@ -6,16 +6,18 @@ def adicao(image1, p1, image2, p2):
     newImage = np.zeros((image1.shape[0], image1.shape[1], 3), np.uint8)
     dst = np.zeros((image1.shape[0], image1.shape[1], 3), np.uint8)
     for i in range(0, image1.shape[0]): #height
+
         for j in range(0, image1.shape[1]): #width
             #print(image1[i, j] * p1 + image2[i, j] * p2)
             newImage[i, j] = image1[i, j] * p1 + image2[i, j] * p2
             dst = cv2.addWeighted(image1, p1, image2, p2, 0.0)
 
-    cv2.imshow('meu', newImage)
-    cv2.imshow('addWeigthed', dst)
+    #cv2.imshow('meu', newImage)
+    #cv2.imshow('addWeigthed', dst)
+    cv2.imshow('soma', cv2.hconcat([newImage, dst]))
     cv2.waitKey(0)
 
 
-image1 = io.imread('images/images.jpeg')
-image2 = np.zeros((image1.shape[0], image1.shape[1], 3), np.uint8)
+image1 = io.imread('images/barco.jpg')
+image2 = io.imread('images/flor.jpg')
 adicao(image1, 0.5, image2, 0.5)
