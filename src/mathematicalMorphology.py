281b4a11a0cb593ac5a55e5aaad80531d5419ca5
KarolineGuckert <karolinesouzaguckert@gmail.com>
2023-04-03 10:22:01 -0300

    inserting comments

--- src/mathematicalMorphology.py
+++ src/mathematicalMorphology.py
@@ -1,76 +1,83 @@
 import numpy as np
 
 
+# Function to erosion the image
 def erosion(image, kernel):
-    rows = image.shape[0]
-    cols = image.shape[1]
+    height = image.shape[0]  # takes the number of columns
+    width = image.shape[1]  # takes the number of lines
 
-    resultImage = np.zeros([rows, cols, 4], np.uint8)
+    resultImage = np.zeros([height, width, 4], np.uint8)
 
-    start = int(len(kernel) / 2)
-    row_element = len(kernel)
-    col_element = len(kernel[0])
+    start = int(len(kernel) / 2)  # initial position
+    widthKernel = len(kernel)  # takes the number of lines
+    heightKernel = len(kernel[0])  # takes the number of columns
 
-    for i in range(start, cols - start):  # lines
-        for j in range(start, rows - start):  # columns
-            pixelImage = image[j][i]
+    for i in range(start, width - start):  # lines
+        for j in range(start, height - start):  # columns
+            pixelImage = image[j][i]  # get pixel of the image
 
-            countKernel = 0
-            countAll = 0
+            countKernel = 0  # reset counter of not nullables in kernel
+            countAll = 0  # reset counter of not nullables in original image and kernel
 
-            for k in range(0, row_element):  # lines
-                for l in range(0, col_element):  # columns
+            for k in range(0, widthKernel):  # lines
+                for l in range(0, heightKernel):  # columns
                     pixelKernel = kernel[k][l]
 
-                    # contabiliza quantos valores no kernel são:
-                    # não nulos
+                    # count how many values are not nullables in kernel
                     if pixelKernel != 0:
                         countKernel += 1
 
-                        # contabiliza valores não nulos coincidentes
-                        # imagem e kernel
-                        positionLine = i - start + k
-                        positionColumn = j - start + l
+                        positionLine = i - start + k  # get line position of pixel
+                        positionColumn = j - start + l  # get column position of pixel
 
+                        # count how many values are not nullables int kernel and original image
                         if image[positionColumn][positionLine][0] != 0:
                             countAll += 1
 
+            # if counters have the same quantities of not nullables change de pixel
             if countKernel == countAll:
                 resultImage[j][i] = pixelImage
 
     return resultImage
 
 
+# Function to dilation the image
 def dilation(image, kernel):
-    rows = image.shape[0]
-    cols = image.shape[1]
+    height = image.shape[0]  # takes the number of columns
+    width = image.shape[1]  # takes the number of lines
 
-    resultImage = np.zeros([rows, cols, 4], np.uint8)
+    resultImage = np.zeros([height, width, 4], np.uint8)
 
-    start = int(len(kernel) / 2)
-    row_element = len(kernel)
-    col_element = len(kernel[0])
+    start = int(len(kernel) / 2)  # initial position
+    row_element = len(kernel)  # takes the number of lines
+    col_element = len(kernel[0])  # takes the number of columns
 
-    for i in range(start, cols - start):  # columns
-        for j in range(start, rows - start):  # lines
-            pixelImage = image[j][i]
+    for i in range(start, width - start):  # lines
+        for j in range(start, height - start):  # columns
+            pixelImage = image[j][i]  # get pixel of the image
 
             if pixelImage[0] != 0:
                 for k in range(0, row_element):  # lines
                     for l in range(0, col_element):  # columns
                         pixelKernel = kernel[k][l]
 
+                        positionLine = i - start + k  # get line position of pixel
+                        positionColumn = j - start + l  # get column position of pixel
+
+                        # if pixelKernel equals 1 change the pixel
                         if pixelKernel == 1:
-                            resultImage[j - start + l][i - start + k] = pixelImage
+                            resultImage[positionColumn][positionLine] = pixelImage
 
     return resultImage
 
 
+# Function to opening the image
 def opening(image, kernel):
     imageErosion = erosion(image, kernel)
     return dilation(imageErosion, kernel)
 
 
+# Function to closing the image
 def closing(image, kernel):
     imageDilation = dilation(image, kernel)
     return erosion(imageDilation, kernel)
